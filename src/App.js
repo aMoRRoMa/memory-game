e4f8f37be143ae7602f32942b8052fd08eb3e975
Roman Gafiatullin <gafiatullin.roman@gmail.com>
2019-10-29 23:24:38 +0300

    Refactor data structure

--- src/App.js
+++ src/App.js
@@ -9,73 +9,87 @@ const CELLS = new Array(CELLS_COUNT).fill(0).reduce(
     [index]: {
       id: index,
       type: (index + 1) % 2 > 0 ? 'red' : 'black',
-      isOpen: false,
     },
   }),
   {},
 );
 
-const Cell = ({ id, type, isOpen, toggleHandler, block }) => (
+const toggleCells = (ids, prev) => {
+  const next = prev;
+  ids.forEach(
+    (id) => {
+      next.has(id) ? next.delete(id) : next.add(id);
+    }
+  );
+  return next;
+};
+
+const updateBlockCells = (ids, prev) => {
+  const next = prev;
+  ids.forEach(
+    (id) => {
+      next.add(id);
+    }
+  );
+  return next;
+};
+
+const isOpen = (id, { openedCells }) => openedCells.has(id);
+const isBlock = (id, { blockCells }) => blockCells.has(id);
+const isFinish = ({ blockCells }) => blockCells.size === CELLS_COUNT;
+const isDirty = ({ blockCells }) => blockCells.size > 0;
+
+const checkingCell = (id, { checkCell, openedCells, blockCells, cells }, setState) => {
+  let newCheckCell, newOpenedCells, newBlockCells = blockCells;
+  
+  const { id: checkingId, type: checkingType } = cells[id];
+  if (checkCell !== null) {
+    if (checkCell === checkingId) {
+      newOpenedCells = toggleCells([checkingId], openedCells);
+      newCheckCell = null
+    } else {
+      const { id: currentId, type: currentType } = cells[checkCell];
+      if (currentType === checkingType) {
+        newOpenedCells = toggleCells([checkingId], openedCells);
+        newCheckCell = null;
+        newBlockCells = updateBlockCells([checkingId, currentId], blockCells)
+      } else {
+        newOpenedCells = toggleCells([currentId], openedCells);
+        newCheckCell = null;
+      }
+    }
+  } else {
+    newOpenedCells = toggleCells([checkingId], openedCells);
+    newCheckCell = checkingId;
+  }
+  
+  setState({
+    cells,
+    checkCell: newCheckCell,
+    openedCells: newOpenedCells,
+    blockCells: newBlockCells,
+  });
+};
+
+const Cell = ({ id, type, isOpen, onClick }) => (
   <div
     className={`Cell ${type} ${isOpen ? 'open' : ''}`}
-    onClick={() => !block && toggleHandler(id)}
+    onClick={() => onClick(id)}
   />
 );
 
 const Game = () => {
-  const initialState = { cells: CELLS, openedCell: null, blockCells: {} };
-  const [{ cells, openedCell, blockCells }, setState] = useState(initialState);
-  const startNewGame = () => setState(initialState);
-  
-  const toggleCells = (ids, cells, withBlock = false) => ({
-    ...cells,
-    ...ids.reduce(
-      (memo, id) => ({
-        ...memo,
-        [id]: {
-          ...cells[id],
-          isOpen: !cells[id].isOpen,
-          block: withBlock
-        },
-      }),
-      {},
-    ),
-  });
-  
-  const checkCell = (id, openedCell, blockCells, cells) => {
-    let newCells, newOpenedCell, newBlockCells = blockCells;
-
-    const { id: checkingId, type: checkingType } = cells[id];
-    if (openedCell !== null) {
-      if (openedCell === checkingId) {
-        newCells = toggleCells([checkingId], cells);
-        newOpenedCell = null
-      } else {
-        const { id: currentId, type: currentType } = cells[openedCell];
-        if (currentType === checkingType) {
-          newCells = toggleCells([checkingId], cells, true);
-          newOpenedCell = null;
-          newBlockCells = { ...blockCells, [checkingId]: true, [currentId]: true }
-        } else {
-          newCells = toggleCells([currentId], cells);
-          newOpenedCell = null;
-        }
-      }
-    } else {
-      newCells = toggleCells([checkingId], cells);
-      newOpenedCell = checkingId;
-    }
-    
-    setState({
-      cells: newCells,
-      openedCell: newOpenedCell,
-      blockCells: newBlockCells,
-    });
+  const initialState = {
+    cells: CELLS,
+    checkCell: null,
+    openedCells: new Set(),
+    blockCells: new Set()
   };
   
-  const isFinish = blockCells => Object.keys(blockCells).length === CELLS_COUNT;
-  const isDirty = blockCells => Object.keys(blockCells).length > 0;
+  const [state, setState] = useState(initialState);
+  const startNewGame = () => setState(initialState);
   
+  const { cells } = state;
   return (
     <Fragment>
       <div className="Game">
@@ -83,14 +97,14 @@ const Game = () => {
           <Cell
             key={cell.id}
             {...cell}
-            block={!!blockCells[cell.id]}
-            toggleHandler={(id) => checkCell(id, openedCell, blockCells, cells)}
+            isOpen={isOpen(cell.id, state)}
+            onClick={id => !isBlock(id, state) && checkingCell(id, state, setState)}
           />)
         )}
       </div>
       <div className="Actions">
-        <button onClick={startNewGame} disabled={!isFinish(blockCells)}>New game</button>
-        <button onClick={startNewGame} disabled={!isDirty(blockCells) || isFinish(blockCells)}>Clear</button>
+        <button onClick={startNewGame} disabled={!isFinish(state)}>New game</button>
+        <button onClick={startNewGame} disabled={!isDirty(state) || isFinish(state)}>Clear</button>
       </div>
     </Fragment>
   );
