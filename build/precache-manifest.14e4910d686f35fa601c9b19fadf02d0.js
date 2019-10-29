715576e6c241557a22b8d23b606551c0bf66d898
Roman Gafiatullin <gafiatullin.roman@gmail.com>
2019-10-29 21:37:48 +0300

    Deploy to github pages

---
+++ build/precache-manifest.14e4910d686f35fa601c9b19fadf02d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df751a8a7e9b86296d9a16b6c9371253",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "e1b400b49964b197b6d0",
+    "url": "/memory-game/static/css/main.c802c28b.chunk.css"
+  },
+  {
+    "revision": "c2ab374c288a7d32b470",
+    "url": "/memory-game/static/js/2.53430267.chunk.js"
+  },
+  {
+    "revision": "e1b400b49964b197b6d0",
+    "url": "/memory-game/static/js/main.c718976c.chunk.js"
+  },
+  {
+    "revision": "e24248335f986e44ed3f",
+    "url": "/memory-game/static/js/runtime-main.baf69392.js"
+  }
+]);
